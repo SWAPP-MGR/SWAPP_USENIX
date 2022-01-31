4058d479cb1bfa8cceb88090e45d4a5bf51119b1
SWAPP-MGR <swapp.mgr@gmail.com>
2022-01-30 18:15:12 -0600

    Init
    Initial commit

---
+++ apps/data_guard.js
@@ -0,0 +1,64 @@
+// App Name: Data Guard
+// Description: Data Guard is to preserve data inside service worker so that
+//              the data won't be accessible in the document context. By doing
+//              so, we can protect the data from being stolen by XSS attackers.
+
+var appObj = new Object();
+
+var request;
+
+function dg_init(){
+    swappInst.storage.createTable("data_guard", "entry", ['value']);
+}
+
+appObj.respMatch = function(fObject){
+    return true;
+}
+
+appObj.respApply = async function(fObject){
+    var transaction = swappInst.storage.db.transaction('data_guard', 'readwrite');
+    var store = transaction.objectStore('data_guard');
+
+    var new_cookies = fObject.getMetadata().headers.get("Set-Cookie");
+    new_cookies = fObject.getMetadata().headers.get("F2F");
+    if(new_cookies){
+        var req = store.get("Cookies");
+        req.onsuccess = function(event){
+            if(req.result){
+                store.put({
+                          entry: "Cookies",
+                          value: req.result["value"] + ";" + new_cookies
+                });
+            } else {
+                store.put({
+                          entry: "Cookies",
+                          value: new_cookies
+                });
+            }
+        }
+    }
+
+    return fObject;
+};
+
+appObj.reqMatch = function(fObject){
+    return true;
+}
+
+appObj.reqApply = function(fObject){
+    var transaction = swappInst.storage.db.transaction('data_guard', 'readwrite');
+    var store = transaction.objectStore('data_guard');
+
+    var req = store.get("Cookies");
+    req.onsuccess = function(event){
+        //fObject.getHeaders().append("Cookies", req.result["value"]);
+    }
+    return fObject;
+}
+
+dg_init();
+
+setTimeout(function () {
+    swappInst.addApp(appObj);
+}, 500)
+
